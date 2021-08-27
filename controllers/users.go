611bcd8e7ddf66d52266198ee7ddb92e18eadf5d
Mahe9025 <it.mahesh90@gmail.com>
2021-08-27 18:37:42 +0530

    same routes different methods applying

--- controllers/users.go
+++ controllers/users.go
@@ -21,9 +21,18 @@ type Users struct {
 	NewView *views.View
 }
 
+// a new user account
+//GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Inside the new method!!")
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+// This is used to process the signup from when a user tries to
+// create a new user account
+//POST Signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "This is a fake message create a new user account")
 }
