68adcd9f7f9beb8797e2868987ebc366ae54629b
Mahe9025 <it.mahesh90@gmail.com>
2021-09-07 19:10:41 +0530

    Go GORM using Database connectvity

--- exp/main.go
+++ exp/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"database/sql"
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -15,26 +18,62 @@ const (
 	dbname   = "goglasses_dev"
 )
 
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+	Color string
+}
+
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("database connected")
 	defer db.Close()
-	var id int
-	err = db.QueryRow(`
-		INSERT INTO users(name, email)
-		VALUES ($1, $2) RETURNING id`, "Test", "Test@yopmail.com").Scan(&id)
+	db.LogMode(true)
+	// db.DropTableIfExists(&User{})
+	db.AutoMigrate(&User{})
+	name, email, color := getInfo()
 
-	if err != nil {
+	u := User{
+		Name:  name,
+		Email: email,
+		Color: color,
+	}
+	if err = db.Create(&u).Error; err != nil {
 		panic(err)
 	}
+	fmt.Println("%+v\n", u)
+	var users []User
+	db.Find(&users)
+	fmt.Println(len(users))
+	fmt.Println(users)
 
-	fmt.Println("id is", id)
-
+	if err := db.Where("email = ?", "xxx@in").First(&u).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			fmt.Println("Records not found.")
+		default:
+			panic(err)
+		}
+	}
+}
+func getInfo() (name, email, color string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What is your name?")
+	name, _ = reader.ReadString('\n')
+	fmt.Println("What is your email?")
+	email, _ = reader.ReadString('\n')
+	fmt.Println("What is your color?")
+	color, _ = reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	email = strings.TrimSpace(email)
+	color = strings.TrimSpace(color)
+	return name, email, color
 }
